41aad7b4a86ad63f96c7036348369eee00ca884c
fisher.gerry <fisher.gerry@mail.ru>
2022-05-13 00:11:21 +0300

    3rd commit

--- edu_express/index.js
+++ edu_express/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import path from 'path'
 import {requestTime, logger} from "./middlewares.js"
+import serverRoutes from './routes/servers.js'
+
 
 const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
@@ -15,6 +17,8 @@ app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(requestTime)
 app.use(logger)
 
+app.use(serverRoutes)
+
 app.get('/', (req, res) => {
     res.render('index', {title: 'Main Page', active: 'main'})
 })
@@ -23,14 +27,6 @@ app.get('/features', (req, res) => {
     res.render('features', {title: 'Features page', active: 'features'})
 })
 
-// app.get('/download_I', (req, res) => {
-//     res.download(path.resolve(__dirname, 'static', 'index.html'))
-// })
-
-// app.get('/download_F', (req, res) => {
-//     //res.send('<h1>HEllo</h1>')
-//     res.download(path.resolve(__dirname, 'static', 'features.html'))
-// })
 
 
 
