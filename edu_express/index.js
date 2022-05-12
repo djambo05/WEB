17113ba1d354a80af625be59a64d7478ecebbd00
fisher.gerry <fisher.gerry@mail.ru>
2022-05-12 23:10:53 +0300

    2nd commit

--- edu_express/index.js
+++ edu_express/index.js
@@ -1,30 +1,38 @@
 import express from "express"
 import path from 'path'
+import {requestTime, logger} from "./middlewares.js"
 
 const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
-//app.use(express.static(path.resolve(__dirname, 'static')))
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs'))
+console.log(app.get('views'))
+
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
 
 app.get('/', (req, res) => {
-    //res.send('<h1>HEllo</h1>')
-    res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
-})
-
-app.get('/download_I', (req, res) => {
-    //res.send('<h1>HEllo</h1>')
-    res.download(path.resolve(__dirname, 'static', 'index.html'))
-})
-
-app.get('/download_F', (req, res) => {
-    //res.send('<h1>HEllo</h1>')
-    res.download(path.resolve(__dirname, 'static', 'features.html'))
+    res.render('index', {title: 'Main Page', active: 'main'})
 })
 
 app.get('/features', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'static', 'features.html'))
+    res.render('features', {title: 'Features page', active: 'features'})
 })
+
+// app.get('/download_I', (req, res) => {
+//     res.download(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+// app.get('/download_F', (req, res) => {
+//     //res.send('<h1>HEllo</h1>')
+//     res.download(path.resolve(__dirname, 'static', 'features.html'))
+// })
+
+
 
 app.listen(PORT, () => {
     console.log(`server on port ${PORT}`)
